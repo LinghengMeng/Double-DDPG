86d7fba2f76ff981b79483a021cdd3784f32a943
Lingheng Meng <lingheng@gra-login1.graham.sharcnet>
2019-02-15 16:05:34 -0500

    add home directory ~

--- tensorboard.sh
+++ tensorboard.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:0        # request GPU "generic resource"
 #SBATCH --cpus-per-task=1   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=8000M        # memory per node
-#SBATCH --time=02:00      # time (DD-HH:MM)
+#SBATCH --time=00:20:00      # time (hours:minutes:seconds)
 
-source tf_gpu/bin/activate
-tensorboard --logdir=../Double_DDPG_Results_no_monitor --host 0.0.0.0
+source ~/tf_gpu/bin/activate
+tensorboard --logdir=../Double_DDPG_Results --host 0.0.0.0
